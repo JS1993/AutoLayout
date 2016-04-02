55c9bbec64db1b8ba76c3bec256722ff5ad70317
susu <28025920@qq.com>
2016-04-02 13:06:14 +0800

    AutoLayout的练习
    AutoLayout自动布局的练习

---
+++ Demo6_AutoLayout/Demo6_AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo6_AutoLayout
+//
+//  Created by  江苏 on 16/3/2.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
